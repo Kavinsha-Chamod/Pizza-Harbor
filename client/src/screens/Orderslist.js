aa77f216c0136ae571fa33df616278c6f62f8956
MrZ2001 <kavinshachamod2001@gmail.com>
2024-02-01 15:38:41 +0530

    Order list added to Admin panel

--- client/src/screens/Orderslist.js
+++ client/src/screens/Orderslist.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+import { Link } from 'react-router-dom';
+import { getUserOrders } from '../actions/orderActions';
 
 export default function Orderslist() {
+
+  const dispatch = useDispatch();
+  const orderstate = useSelector((state) => state.getUserOrdersReducer) 
+  const {orders, error, loading} = orderstate;
+
+  useEffect(()=>{
+    dispatch(getUserOrders());
+  },[])
   return (
     <div>
       <h1>Orders List</h1>
+      {loading && (<Loading/>)}
+      {error && (<Error error="Something went wrong !"/>)}
+      <table className='table table-striped table-bordered'>
+      <thead>
+      <tr>
+      <th>Order Id</th>
+      <th>Email</th>
+      <th>User Id</th>
+      <th>Amount</th>
+      <th>Date</th>
+      <th>Status</th>
+      </tr>
+      </thead>
+      <tbody>
+        {orders && orders.map(order=>{
+          return <tr>
+            <td>{order._id}</td>
+            <td>{order.email}</td>
+            <td>{order.userid}</td>
+            <td>LKR.{order.orderAmount}</td>
+            <td>{order.createdAt.substring(0,10)}</td>
+            <td>
+               {order.isDelivered ? (<h1>Delivered</h1>):
+              (<button className='btn'>Deliver</button>)
+              }
+            </td>
+          </tr>
+        })}
+      </tbody>
+      </table>
     </div>
   );
 }
