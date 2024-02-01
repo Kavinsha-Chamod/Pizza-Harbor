29316b96b70b4944d7881e77dc69c665ee52ea36
MrZ2001 <kavinshachamod2001@gmail.com>
2024-02-01 15:19:21 +0530

    Remove pizza added in Admin panel

--- routes/pizzasRoute.js
+++ routes/pizzasRoute.js
@@ -58,4 +58,15 @@ router.post("/editpizza", async (req,res)=>{
   }
 })
 
+router.post("/deletepizza", async (req,res) =>{
+  const pizzaid = req.body.pizzaid
+  try {
+    await Pizza.findOneAndDelete({_id: pizzaid})
+    res.send('Pizza deleted successfully !')
+    
+  } catch (error) {
+    return res.status(400).json({message: error})
+  }
+})
+
 module.exports = router;
